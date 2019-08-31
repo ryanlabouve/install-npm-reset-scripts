bd2a07b83191aa012aaddd473b4b2c197c3c4477
Ryan LaBouve <imryanlabouve@gmail.com>
2019-08-30 20:59:54 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const defaultScripts = {
+  "clean":
+    "rm -rf node_modules bower_components dist && yarn clean:system_temp",
+  "clean:system_temp":
+    "find /var/folders -name 'broccoli-*' -type d -print 2> /dev/null | xargs rm -rf",
+  "reset": "npm run clean clean && npm install"
+};
+
+function index() {
+  let file;
+  let packageJson;
+
+  try {
+    file = fs.readFileSync('package.json');
+    packageJson = JSON.parse(file.toString()); 
+  } catch {
+    console.log("Error reading package.json");
+  }
+
+  packageJson.scripts = Object.assign({}, packageJson.scripts, defaultScripts);
+  debugger;
+}
+
+index();
